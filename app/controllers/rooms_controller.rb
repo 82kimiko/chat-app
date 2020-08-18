c0f7b972f5fe5c1e0f6cf42dae3100e36d1c9516
82kimiko <kimisabrou407@gmail.com>
2020-08-18 09:38:06 +0900

    メッセージ送信機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,18 +2,17 @@ class RoomsController < ApplicationController
 
   def index
   end
-  
-  def new
-    @room = Room.new
-  end
 
   def create
-    @room = Room.new(room_params)
-    if @room.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    @message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 
   private
